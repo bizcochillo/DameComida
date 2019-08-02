b14ad69560b95b40477962c36a607621f4ab14ab
Palma Palomo, Abelardo <abelardo.palma@jungheinrich.es>
2019-08-02 12:56:08 +0200

    Basic navigation (first level)

---
+++ src/DameComidaMobile/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DameComidaMobile
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnTrackMeal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new TrackMeal());
+        }
+
+        private async void OnShowMeal_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new ShowMeal());
+        }
+
+        private async void OnManage_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new Manage());
+        }
+    }
+}
